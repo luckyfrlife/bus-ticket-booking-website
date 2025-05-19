36383af15772bf187be987b35603bc88983bac77
Lucky <luckyagboola@gmail.com>
2025-05-19 17:42:32 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_ETH || "/bus-ticket-booking-website",
   server: {
     port: 3000,
   }
